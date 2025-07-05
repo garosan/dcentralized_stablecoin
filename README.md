bb82088af59b62a770f83232f417d72fd647c485
Garosan <garosanchez27@gmail.com>
2025-07-04 18:33:09 -0600

    First commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Cyfrin DeFi StableCoin Project
+
+Characteristics:
+
+- Relative Stability: Anchored or Pegged -> $-00
+  - Chainlink Price feed.
+  - Set a function to exchange ETH & BTC -> $$$
+- Stability Mechanism (Minting): Algorithmic (Decentralized)
+  - People can only mint the stablecoin with enough collateral (coded)
+- Collateral: Exogenous (Crypto)
+  - wETH
+  - wBTC
---
+++ src/DecentralizedStablecoin.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.26;
+
+// Layout of Contract:
+// version
+// imports
+// interfaces, libraries, contracts
+// errors
+// Type declarations
+// State variables
+// Events
+// Modifiers
+// Functions
+
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+// view & pure functions
+
+/*
+ * @title: DecentralizedStableCoin
+ * @author: Garosan
+ * Collateral: Exogenous (ETH & BTC)
+ * Minting: Algorithmic
+ * Relative Stability: Pegged to USD
+ *
+ * This is the contract meant to be governed by DSCEngine. This contract is just the ERC20 implementation of our stablecoin system.
+ */
+
+contract DecentralizedStableCoin {}
